3e10e869130cab41ada6e1b781cd4a5b028dbb6c
fland <fland@users.noreply.github.com>
2019-03-18 20:09:35 +0100

    +initial maven project skeleton

---
+++ src/main/java/org/loan_calculator/cli/Main.java
@@ -0,0 +1,15 @@
+package org.loan_calculator.cli;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Maksym Bondarenko
+ * @version 1.0 18.03.19
+ */
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        log.info("Hello world!");
+    }
+}
